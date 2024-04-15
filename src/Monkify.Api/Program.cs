fe6678b1d7cd1552fd0f416793b27b1d490ba833
marcusruas <marcusruas.andrade@gmail.com>
2024-04-15 15:48:10 -0300

    Adicionado IP Rate Limit

--- src/Monkify.Api/Program.cs
+++ src/Monkify.Api/Program.cs
@@ -14,8 +14,19 @@ using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using static Monkify.Infrastructure.DependencyInjection;
+using System.Configuration;
+using AspNetCoreRateLimit;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOptions();
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitingPolicies"));
+builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+builder.Services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();  // Adiciona esta linha
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
 
 builder.Services.AddControllers(options =>
 {
@@ -52,6 +63,8 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+app.UseIpRateLimiting();
 
 ApplyMigrations(app);
 
