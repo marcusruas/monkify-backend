92609edd0e0a19578f4d40523be72fdb966f1ddc
marcusruas <marcusruas.andrade@gmail.com>
2024-04-15 14:39:17 -0300

    Corrigido script

--- sqlserver-commands.sh
+++ sqlserver-commands.sh
@@ -13,7 +13,7 @@ docker exec df77e82edfa44f78846140c82a689de83fdf9cb942e1cfb2d00d7b505592977d /op
 docker exec df77e82edfa44f78846140c82a689de83fdf9cb942e1cfb2d00d7b505592977d /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Monkify@123" -Q "
 	USE MONKIFY
 	GO
-
+	
 	INSERT INTO SESSIONPARAMETERS VALUES
-	(NEWID(), 2, 1, 1, GETDATE(), NULL, 4, 1, 2)
+	(NEWID(), 2, 2, 1, 4, 1, 1, GETDATE(), NULL)
 "
